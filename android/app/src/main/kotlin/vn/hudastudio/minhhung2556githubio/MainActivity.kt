1f5de2a262ff7828a8b9085a979d289b014de9ed
Hung Luong Do Minh <hung.luongdm@homecredit.vn>
2021-05-14 10:17:26 +0700

    add page1

--- android/app/src/main/kotlin/vn/hudastudio/minhhung2556githubio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package vn.hudastudio.minhhung2556githubio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:minhhung2556/utils/ui_utils.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lương Đỗ Minh Hưng',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -19,37 +25,114 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key key,
-  }) : super(key: key);
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+const _kScreenPadding = 24.0;
+
 class _MyHomePageState extends State<MyHomePage> {
+  var _scrollController = ScrollController();
+  late Size _screenSize;
+  double _page1Value = 0;
+
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      _page1Value =
+          max(0.0, min(1.0, _scrollController.offset / _screenSize.height));
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        controller: _scrollController,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildPage1(),
+            Container(
+              height: _screenSize.height * 6,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage1() {
+    return Transform.translate(
+      offset: Offset(0, _page1Value * _screenSize.height),
+      child: SizedBox(
+        height: _screenSize.height,
+        child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "This is me",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          children: [
+            const SizedBox(width: _kScreenPadding),
+            Expanded(
+              child: Transform.translate(
+                offset: Offset(-_page1Value * _screenSize.width / 2, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            shadows: [kBoxShadow],
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "I'm   ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Lương Đỗ Minh Hưng",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "\nMaking application development be more romantic.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 150,
-              backgroundImage: Image.network(
-                "https://scontent.fsgn2-2.fna.fbcdn.net/v/t1.0-9/68493367_2471086616267942_1237442273399013376_n.jpg?_nc_cat=102&_nc_sid=85a577&_nc_ohc=R-XV5dAf5ZgAX-PmvaF&_nc_ht=scontent.fsgn2-2.fna&oh=8976026488ea267381a4911d5146b186&oe=5EE1E507",
-              ).image,
+            const SizedBox(width: _kScreenPadding * 2),
+            Expanded(
+              child: Transform.translate(
+                offset: Offset(_page1Value * _screenSize.width / 2, 0),
+                child: ShadowWidget(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      "avatar.jpeg",
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(width: _kScreenPadding),
           ],
         ),
       ),
