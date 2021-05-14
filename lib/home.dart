3dd8aa243453978a2ab0f38107a93e5a92962772
Hung Luong Do Minh <hung.luongdm@homecredit.vn>
2021-05-15 00:45:55 +0700

    update page1

--- lib/home.dart
+++ lib/home.dart
@@ -385,88 +385,105 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     return Transform.translate(
       offset: Offset(0, _page1Value * _screenH),
       child: Container(
-        color: kColorTween12.transform(av),
         width: _screenW,
         height: _screenH,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            const SizedBox(width: kScreenPadding),
-            Expanded(
-              child: Transform.translate(
-                offset: Offset(-dx, 0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FittedBox(
-                      child: RichText(
-                        text: TextSpan(
-                          style: TextStyle(
-                            shadows: [kBoxShadow],
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "Hi! I'm ",
-                              style: TextStyle(
-                                fontSize: 30,
-                              ),
-                            ),
-                            TextSpan(
-                              text: kMyName,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Text(
-                      kMyTitle,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: kColorTween12.transform(av),
+                width: _screenW,
+                height: _screenH * 0.8,
               ),
             ),
-            const SizedBox(width: kScreenPadding * 2),
-            Expanded(
-              child: Transform.translate(
-                offset: Offset(dx, 0),
-                child: RotationTransition(
-                  turns: TweenSequence<double>([
-                    TweenSequenceItem(
-                        tween: Tween<double>(
-                            begin: kDegToRad * 5.0, end: kDegToRad * -5.0),
-                        weight: 0.5),
-                    TweenSequenceItem(
-                        tween: Tween<double>(begin: kDegToRad * -5, end: 0.0),
-                        weight: 0.5),
-                  ])
-                      .chain(CurveTween(curve: Curves.bounceInOut))
-                      .animate(_page1AC),
-                  alignment: Alignment.topCenter,
-                  child: ShadowWidget(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(kBorderRadiusSmall),
-                      child: Opacity(
-                        opacity: max(0.0, 1 - _page1Value * 2),
-                        child: Image.asset(
-                          Assets.assetAvatar,
-                          fit: BoxFit.fitWidth,
+            Align(
+              alignment: Alignment.center,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(width: kScreenPadding),
+                  Expanded(
+                    child: Transform.translate(
+                      offset: Offset(-dx, 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FittedBox(
+                            child: RichText(
+                              text: TextSpan(
+                                style: TextStyle(
+                                  shadows: [kBoxShadow],
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "Hi! I'm ",
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: kMyName,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Text(
+                            kMyTitle,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Divider(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: kScreenPadding * 2),
+                  Expanded(
+                    child: Transform.translate(
+                      offset: Offset(dx, 0),
+                      child: RotationTransition(
+                        turns: TweenSequence<double>([
+                          TweenSequenceItem(
+                              tween: Tween<double>(
+                                  begin: kDegToRad * 5.0,
+                                  end: kDegToRad * -5.0),
+                              weight: 0.5),
+                          TweenSequenceItem(
+                              tween: Tween<double>(
+                                  begin: kDegToRad * -5, end: 0.0),
+                              weight: 0.5),
+                        ])
+                            .chain(CurveTween(curve: Curves.bounceInOut))
+                            .animate(_page1AC),
+                        alignment: Alignment.topCenter,
+                        child: ShadowWidget(
+                          child: ClipRRect(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusSmall),
+                            child: Opacity(
+                              opacity: max(0.0, 1 - _page1Value * 2),
+                              child: Image.asset(
+                                Assets.assetAvatar,
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                  const SizedBox(width: kScreenPadding),
+                ],
               ),
-            ),
-            const SizedBox(width: kScreenPadding),
+            )
           ],
         ),
       ),
