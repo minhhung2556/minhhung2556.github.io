023c0301d151d100cd9144437cf092ba6240c08b
Hung Luong Do Minh <hung.luongdm@homecredit.vn>
2021-05-29 15:23:40 +0700

    add content seo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "67f3aa081b4fa6fa8e12ee3466b4d7f4",
-"index.html": "849b2ce0b1871e63a5e265f2785400b2",
-"/": "849b2ce0b1871e63a5e265f2785400b2",
-"main.dart.js": "3a911e05f9ebd97379843541f2656662",
+"index.html": "9467848fd33c4cad2fa4420c36ba246f",
+"/": "9467848fd33c4cad2fa4420c36ba246f",
+"main.dart.js": "53b13bbaf6c2bfffabd4f9f4dda7374c",
 "favicon.png": "1febf57da596ee5e57bab0f2c232d5df",
 "icons/Icon-192.png": "2044a82c479c3cbf19829b6ce646540e",
 "icons/Icon-512.png": "d6c3a21dd684231969dbe686ca415a50",
